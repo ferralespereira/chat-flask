5b9acb65f44a3ae30b038a926e6dce5cd92dfad9
javier ferrales <ferralespereira@gmail.com>
2022-06-11 18:39:12 -0400

    working with tsc

--- static/js/session.js
+++ static/js/session.js
@@ -1,32 +1,24 @@
 var socket = io.connect('http://' + document.domain + ':' + location.port);
-
-socket.on( 'connect', function() {
+socket.on('connect', function () {
     socket.emit('my event', {
         data: 'User Connected'
     });
 });
-
-function sendMessage(e){
-  event.preventDefault();
-
-  let user_name  = document.getElementById('username');
-  let message = document.getElementById('message');
-  
-  socket.emit( 'my event', {
-    user_name : user_name.value,
-    message : message.value
-  });
-
+function sendMessage() {
+    event.preventDefault();
+    var user_name = document.getElementById('username');
+    var message = document.getElementById('message');
+    socket.emit('my event', {
+        user_name: user_name.value,
+        message: message.value
+    });
 }
-
-socket.on('my response', function(msg) {
+socket.on('my response', function (msg) {
     console.log(msg);
-    if(typeof msg.user_name !== 'undefined'){
-
-      let message_holder = document.getElementById('message-holder');
-      let first_text     = document.getElementById('first-text');
-      
-      first_text.innerHTML = "";
-      message_holder.innerHTML += '<div><b style="color: #000">'+msg.user_name+'</b> '+msg.message+'</div>';
+    if (typeof msg.user_name !== 'undefined') {
+        var message_holder = document.getElementById('message-holder');
+        var first_text = document.getElementById('first-text');
+        first_text.innerHTML = "";
+        message_holder.innerHTML += '<div><b style="color: #000">' + msg.user_name + '</b> ' + msg.message + '</div>';
     }
 });
